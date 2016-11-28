85b0dfbf8b38837902a83ee9d8d4e2d8f4110834
Brent Shay Lopez <brentshay.lopez@gmail.com>
2016-11-28 14:28:03 -0500

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name messageCraftApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the messageCraftApp
+ */
+angular.module('messageCraftApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
