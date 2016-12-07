cc724e80e30982b2611b0002791ea335800a74d1
Brent Shay Lopez <brentshay.lopez@gmail.com>
2016-12-07 11:10:09 -0500

    Fix stateEvents import and other minor fixed

--- app/scripts/services/postservice.js
+++ app/scripts/services/postservice.js
@@ -20,25 +20,25 @@ angular.module('messageCraftApp')
 		},
 		{
 			senderName: 'Ronald K. Drumpf',
-			senderPicPath: 'http://lorempixel.com/201/201/',
-			content: 'Where you will try to get as much free media as you possibly can! #MakeMessageCraftGreatAgain',
+			senderPicPath: 'http://lorempixel.com/200/200/',
+			content: 'Where you will try to get as much free media as you possibly can!',
 			timestamp: '01/01/1999 00:00:00'
 		},
 		{
 			senderName: 'Elyk Ykcinuhob',
-			senderPicPath: 'http://lorempixel.com/202/202/',
+			senderPicPath: 'http://lorempixel.com/200/200/',
 			content: 'Things will happen!',
 			timestamp: '01/01/1999 00:00:00'
 		},
 		{
 			senderName: 'Paul Itical',
-			senderPicPath: 'http://lorempixel.com/203/203/',
+			senderPicPath: 'http://lorempixel.com/200/200/',
 			content: 'And how you respond is sure to get the attention of others.',
 			timestamp: '01/01/1999 00:00:00'
 		},
 		{
 			senderName: 'Bloop Blop',
-			senderPicPath: 'http://lorempixel.com/204/204/',
+			senderPicPath: 'http://lorempixel.com/200/200/',
 			content: 'Don\'t screw it up!',
 			timestamp: '01/01/1999 00:00:00'
 		}
